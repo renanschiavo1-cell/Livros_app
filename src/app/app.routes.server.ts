63b4fded1444a26b526c75fdbbc86da0d0cf12f1
Renan Schiavo <renan.schiavo1@gmail.com>
2026-08-31 22:44:49 -0300

    commit

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,11 +2,7 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: 'livros/:id',
-    renderMode: RenderMode.Client
-  },
-  {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Client
   }
 ];
